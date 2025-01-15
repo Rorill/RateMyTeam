541c0009939f3356c93d7d3202b4384b8b88cef5
Rorill <guillaumeherold@gmail.com>
2025-01-15 09:08:29 +0100

    corrected a few things

--- assets/app.js
+++ assets/app.js
@@ -9,8 +9,3 @@ import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
-
-function burger_click() {
-    const burgerMenu = document.querySelector('.burger-menu');
-    burgerMenu.classList.toggle('show');
-}
